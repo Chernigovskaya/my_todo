606e0c543e0208d323d350971a2a5ebdfa7b77cb
Chernigovskaya <m.n.chernigov@gmail.com>
2022-08-07 17:19:12 +0700

    add hesh

--- todo_project/todo/usersapp/admin.py
+++ todo_project/todo/usersapp/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from usersapp.models import UsersTodo
 
-admin.site.register(UsersTodo)
+admin.site.register(UsersTodo, UserAdmin)
+
+
+
